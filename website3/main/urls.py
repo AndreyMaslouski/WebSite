6034257284e85b429186a158aae1f3e519e434c9
Andrei Mas <masloyskiandrey@gmail.com>
2024-01-07 17:41:08 +0300

    Создал новый urlpatterns

--- website3/main/urls.py
+++ website3/main/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name= 'home'),
+    path('about-us', views.about, name = 'about'),
+    path('help-me', views.help),
+    path('create', views.about, name = 'create'),
+    path('draw', views.about, name = 'draw')
 
 ]
